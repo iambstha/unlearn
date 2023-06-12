5c003d99f62c3d675ce65d421ba8a4a268d0d97e
iambstha <iambstha@gmail.com>
2023-06-12 20:48:51 +0545

    Minor changes

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${jakarta.className} min-h-[100vh]`}  >
+      <body className={`${lato.className} min-h-[100vh]`}  >
         <Header />
         {children}
         <Footer />
--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,16 @@
 import getPost from '@/graphquery/getPost'
+import getPosts from '@/graphquery/getPosts'
 
 async function Post({ params }) {
     const post = await getPost(params)
+    const posts = await getPosts()
+    console.log(posts)
+    console.log(posts.map(p => (p.tags.includes('cloudfactory'))))
     return (
         <>
             {
                 post ?
-                    <div className=' flex w-full justify-center items-center ' >
+                    <div className=' flex flex-col w-full justify-center items-center ' >
                         <div className=' flex flex-col justify-center items-start p-4 md:p-2 w-full md:w-4/5  ' >
                             <h1 className=' text-2xl md:text-4xl lg:text-6xl text-slate-800 font-semibold text-left w-full py-4 ' >{post.title}</h1>
                             <span className=' w-full text-slate-400 text-left text-sm md:text-base lg:text-lg py-2 ' >{post.subtitle}</span>
