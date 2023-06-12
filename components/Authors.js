e30fb677f628237fe8a2ce5d83631011f7677f01
iambstha <iambstha@gmail.com>
2023-06-12 18:55:43 +0545

    Author indexing added

--- components/Authors.js
+++ components/Authors.js
@@ -8,6 +8,7 @@ export default async function Authors() {
             <table className=" w-2/3 border-[1px] border-slate-200" >
                 <thead>
                     <tr className=" border-[1px] border-slate-200 " >
+                        <th className=" p-2 border-[1px] border-slate-200 bg-slate-100 ">S. No.</th>
                         <th className=" p-2 border-[1px] border-slate-200 bg-slate-100 ">Name</th>
                         <th className=" p-2 bg-slate-100 ">Biography</th>
                     </tr>
@@ -15,6 +16,7 @@ export default async function Authors() {
                 <tbody>
                     {authors.map(author => (
                         <tr key={author.id} >
+                        <td className=" border-[1px] border-slate-200 bg-slate-50 p-2 text-center">{authors.indexOf(author) + 1}</td>
                             <td className=" border-[1px] border-slate-200 bg-slate-50 p-2 ">{author.name}</td>
                             <td className=" border-[1px] border-slate-200 bg-slate-50 p-2 ">{author.biography}</td>
                         </tr>
